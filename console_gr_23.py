572d563d6791d5b944cdd4b0ae41362d9f3d57cd
Gaillard Florian <florian.gaillard@student.unamur.be>
2022-11-06 11:24:33 +0100

    corp consale et gamepad

---
+++ console_gr_23.py
@@ -0,0 +1,72 @@
+import math
+import random
+
+import radio
+import microbit
+
+
+# definition of functions
+def get_message():
+    """Wait and return a message from another micro:bit.
+
+    Returns
+    -------
+    message: message sent by another micro:bit (str)
+
+    """
+
+    message = None
+    while message == None:
+        microbit.sleep(250)
+        message = radio.receive()
+
+    return message
+
+
+..........
+..........
+..........
+
+# settings
+group_id = ..........
+
+# setup radio to receive orders
+radio.on()
+radio.config(group=group_id)
+
+# create empty board + available pieces
+..........
+
+# loop until game is over
+nb_dropped_pieces = 0
+game_is_over = False
+
+while not game_is_over:
+    # show score (number of pieces dropped)
+    microbit.display.show(nb_dropped_pieces)
+
+    # create a new piece in the top left corner
+    ..........
+
+    # check if the new piece collides with dropped pieces
+    game_is_over = ..........
+
+    if not game_is_over:
+        # ask orders until the current piece is dropped
+        piece_dropped = False
+        while not piece_dropped:
+            # send state of the board to gamepad (as a string)
+            radio.send(..........)
+
+            # wait until gamepad sends an order
+            order = get_message()
+
+            # execute order (drop or move piece)
+            ..........
+
+        # wait a few milliseconds and clear screen
+        microbit.sleep(500)
+        microbit.display.clear()
+
+# tell that the game is over
+microbit.display.scroll('Game is over', delay=100)
---
+++ gamepad_gr_23.py
@@ -0,0 +1,58 @@
+import radio
+import microbit
+
+
+# definition of functions
+def get_message():
+    """Wait and return a message from another micro:bit.
+
+    Returns
+    -------
+    message: message sent by another micro:bit (str)
+
+    """
+
+    message = None
+    while message == None:
+        microbit.sleep(250)
+        message = radio.receive()
+
+    return message
+
+
+..........
+..........
+..........
+
+# settings
+group_id = ..........
+
+# setup radio to receive/send messages
+radio.on()
+radio.config(group=group_id)
+
+# loop forever (until micro:bit is switched off)
+while True:
+    # get view of the board
+    view = get_message()
+
+    # clear screen
+    microbit.display.clear()
+
+    # show view of the board
+    ..........
+
+    # wait for button A or B to be pressed
+    while not (microbit.button_a.is_pressed() or microbit.button_b.is_pressed()):
+        microbit.sleep(50)
+
+    if microbit.button_a.is_pressed():
+        # send current direction
+        ..........
+        ..........
+        ..........
+
+        radio.send(..........)
+    elif microbit.button_b.is_pressed():
+        # notify that the piece should be dropped
+        radio.send(..........)
