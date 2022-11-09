69603fac81802c9e07b18ba78079ea4102db1aa2
Gaillard Florian <florian.gaillard@student.unamur.be>
2022-11-09 16:20:03 +0100

    execute order new

--- console_gr_23.py
+++ console_gr_23.py
@@ -33,12 +33,35 @@ def is_collision():
     for x in range(len(brick)):
         for y in range(len(brick)):
             if not collision:
-                if brick[x][y] > 0 and board[x][y] > 0:
+                if brick[x][y] != 0 and board[x][y] != 0:
                     collision = True
     return collision
 
-def moove(direction):
-    ...
+
+def move(direction, coord_x, coord_y):
+    """move the brick through the board
+    parameter:
+    ---------------
+    direction: direction th brick will move (str)
+    coord_x: coordinates x of the brick (int)
+    coord_y: coordinates y of the brick (int)
+
+    return:
+    ---------------
+
+    """
+    if direction == 'up':
+        coord_x -= 1
+        if not board[coord_x][coord_y]  == 0:
+            if
+    if direction == 'down':
+        coord_x += 1
+    if direction == 'left':
+        coord_y -= 1
+    if direction == 'right':
+        coord_y += 1
+    return coord_x, coord_y
+
 
 # settings
 group_id = 23
@@ -49,7 +72,7 @@ radio.config(group=group_id)
 
 # create empty board + available pieces
 board = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
-bricks = [[9, 9], [9, 0]], [[9, 9], [0, 9]], [[9, 9], [9, 9]], [[9, 9], [0, 0]], [[9, 0], [0, 0]], [[9, 0], [9, 0]], []
+bricks = [[9,9],[9,0]],[[9,9],[0,9]],[[9,9],[9,9]],[[9,9],[0,0]],[[9,0],[0,0]],[[9,0],[9,0]],[[9,0],[9,9]]
 
 # loop until game is over
 nb_dropped_pieces = 0
@@ -64,7 +87,8 @@ while not game_is_over:
     for x in range(len(brick)):
         for y in range(len(brick)):
             microbit.display.set_pixel(x, y, 9)
-
+    coord_x = 0
+    coord_y = 0
     # check if the new piece collides with dropped pieces
     is_collision()
 
@@ -75,13 +99,17 @@ while not game_is_over:
             # send state of the board to gamepad (as a string)
             board_str = ''
             board_str = board_str.join(board)
-            radio.send()
+            radio.send(board_str)
 
             # wait until gamepad sends an order
             order = get_message()
 
             # execute order (drop or move piece)
-            moove(order)
+            if order == 'drop':
+                piece_dropped = True
+                nb_dropped_pieces += 1
+            else:
+                coord_x, coord_y = move(order, coord_x, coord_y)
 
         # wait a few milliseconds and clear screen
         microbit.sleep(500)
